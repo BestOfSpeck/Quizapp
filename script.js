5a54a45b19be195ea6438e880ea4e630d4e28d07
BestOfSpeck <tom.kuestermann76@gmail.com>
2022-10-13 13:05:57 +0200

    add question/answer array

---
+++ script.js
@@ -0,0 +1,41 @@
+let questions = [{
+        "question": "Was bedeutet HTML ausgeschrieben?",
+        "answer_1": "Hypertext Main Logic",
+        "answer_2": "Hausgemachte Tanzende Mäuse Lampe",
+        "answer_3": "Highend Text Manager Languane",
+        "answer_4": "Hypertext Markup Language",
+        "right_answer": 4
+    },
+    {
+        "question": "Wofür steht das www bei vielen Internetseiten?",
+        "answer_1": "World Wide Web",
+        "answer_2": "Was Wissen Welpen",
+        "answer_3": "World Wide Walrus",
+        "answer_4": "Welt weites Welken",
+        "right_answer": 1
+    },
+    {
+        "question": "Wie wird eine Webseite designed?",
+        "answer_1": "Mit CSS",
+        "answer_2": "Mit Python",
+        "answer_3": "Mit Java",
+        "answer_4": "Mit PHP",
+        "right_answer": 1
+    },
+    {
+        "question": "Wofür sthet CSS",
+        "answer_1": "",
+        "answer_2": "Conduited System Server",
+        "answer_3": "Controled Server System",
+        "answer_4": "Cascading Style Sheets",
+        "right_answer": 4
+    },
+    {
+        "question": "Wofür steht a href in HTML?",
+        "answer_1": "Um ein Bild einzubinden",
+        "answer_2": "Für einen Link",
+        "answer_3": "Um PHP zu Importieren",
+        "answer_4": "Für Formatierungen wie Fett und Kursiv schreiben",
+        "right_answer": 4
+    },
+];
