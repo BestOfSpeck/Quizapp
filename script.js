f36b66bba7af896371f42c75f0d897221036464d
BestOfSpeck <tom.kuestermann76@gmail.com>
2022-10-17 19:36:46 +0200

    presave

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ let questions = [{
     },
     {
         "question": "Wofür sthet CSS",
-        "answer_1": "",
+        "answer_1": "Control Station Supervisor",
         "answer_2": "Conduited System Server",
         "answer_3": "Controled Server System",
         "answer_4": "Cascading Style Sheets",
@@ -36,6 +36,35 @@ let questions = [{
         "answer_2": "Für einen Link",
         "answer_3": "Um PHP zu Importieren",
         "answer_4": "Für Formatierungen wie Fett und Kursiv schreiben",
-        "right_answer": 4
-    },
+        "right_answer": 2
+    }
 ];
+
+let currentQuestion = 0;
+
+
+function init() {
+    document.getElementById('all-questions').innerHTML = questions.length;
+
+    showQuestion()
+}
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+
+    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestinaNumber = selection.slice(-1);
+
+
+    console.log('selected questiin number is ', selectedQuestinaNumber)
+    console.log('currend question is ', question)
+    console.log('Selected answer is ', selection)
+}
